7d19aa86ac343189ece8fd97653a09460edb574a
themaximsuper <41567448+themaximsuper@users.noreply.github.com>
2022-02-25 10:40:36 +0300

    Updated main.py

--- main.py
+++ main.py
@@ -1,88 +1,97 @@
 from time import sleep
 import tkinter
+from unittest import expectedFailure
 
+commands = [
+    (
+        """blablabla""",
+        {"1.1":"a", "1.2":"b", "1.3":"c", "1.4":"d", "1.5":"e"}
+    ),
 
-class iteration():
-    commands = [
-        (
-            """blablabla""",
-            {"1.1":"a", "1.2":"b", "1.3":"c", "1.4":"d", "1.5":"e"}
-        ),
+    (
+        """bleblebe""",
+        {"1.1.1":"aa", "1.1.2":"ab", "1.1.3":"ac"}
+    ),
 
-        (
-            """bleblebe""",
-            {"1.1.1":"aa", "1.1.2":"ab", "1.1.3":"ac"}
-        ),
+    (
+        """blublublu""",
+        {"1.2.1":"ba", "1.2.2":"bb", "1.2.3":"bc"}
+    ),
 
-        (
-            """blublublu""",
-            {"1.2.1":"ba", "1.2.2":"bb", "1.2.3":"bc"}
-        ),
-
-        (
-            """blyblybly""",
-            {"1.3.1":"ca", "1.3.2":"cb", "1.3.3":"cc"}
-        ),
+    (
+        """blyblybly""",
+        {"1.3.1":"ca", "1.3.2":"cb", "1.3.3":"cc"}
+    ),
             
-        (
-            """blobloblo""",
-            {"1.4.1":"da", "1.4.2":"db", "1.4.3":"dc"}
-        ),
+    (
+        """blobloblo""",
+        {"1.4.1":"da", "1.4.2":"db", "1.4.3":"dc"}
+    ),
 
-        (
-            """bliblibli""",
-            {"1.5.1":"ea", "1.5.2":"eb", "1.5.3":"ec"}
-        )
-    ]  
-    #То, что было изначально
-    def __init__(self):
-        pass
-
-        def __call__(self, number):
-        
-            self.number = number
-            self.obj = self.commands[self.number]
-            
-            print(self.obj[0]+ '\n')
-            for key in self.obj[1]:
-                print(key)
-            while True:
-                self.answer = input("Введите команду: ")
-                try:
-                    print(self.obj[1][self.answer])
-                except KeyError:
-                    print("Ошибка! Введите нужную команду из списка.")
-                    continue
-                break
-            if self.number == 0:
-                if self.answer == "1.1":
-                    self.output = 1
-                elif self.answer == "1.2":
-                    self.output = 2
-                elif self.answer == "1.3":
-                    self.output = 3
-                elif self.answer == "1.4":
-                    self.output = 4
-                elif self.answer == "1.5":
-                    self.output = 5
-            else:
-                self.output = self.answer
-            return self.output
+    (
+        """bliblibli""",
+        {"1.5.1":"ea", "1.5.2":"eb", "1.5.3":"ec"}
+    )
+]  
 
 win = tkinter.Tk()
 h = 540
-w = 700
-
+w = 800
+global number
+number = 0
 win.geometry(f"{w}x{h}")
 win.title('Бот Валера')
 win.resizable(False, True)
 
 def get_entry():
+    global number
     to_send = message.get()
     text.config(state = 'normal')
     if to_send:
         text.insert(1.0, '\n' f'Вы: {to_send}')
         #if to_send in iteration.call.commands:
+        
+        if number == 0:
+            if to_send == "1.1":
+                text.insert(1.0, "Валерий: "+commands[0][1][to_send]+"\n")
+                number = 1
+                text.insert(1.0, "Валерий: "+commands[number][0]+"\n")
+                text.insert(1.0, "Валера: выберите нужную команду:\n")
+                for key in commands[number][1]:
+                    text.insert(1.0, key+"\n")
+            elif to_send == "1.2":
+                text.insert(1.0, "Валерий: "+commands[0][1][to_send]+"\n")
+                number = 2
+                text.insert(1.0, "Валерий: "+commands[number][0]+"\n")
+                text.insert(1.0, "Валера: выберите нужную команду:\n")
+                for key in commands[number][1]:
+                    text.insert(1.0, key+"\n")
+            elif to_send == "1.3":
+                text.insert(1.0, "Валерий: "+commands[0][1][to_send]+"\n")
+                number = 3
+                text.insert(1.0, "Валерий: "+commands[number][0]+"\n")
+                text.insert(1.0, "Валера: выберите нужную команду:\n")
+                for key in commands[number][1]:
+                    text.insert(1.0, key+"\n")
+            elif to_send == "1.4":
+                text.insert(1.0, "Валерий: "+commands[0][1][to_send]+"\n")
+                number = 4
+                text.insert(1.0, "Валерий: "+commands[number][0]+"\n")
+                text.insert(1.0, "Валера: выберите нужную команду:\n")
+                for key in commands[number][1]:
+                    text.insert(1.0, key+"\n")
+            elif to_send == "1.5":
+                text.insert(1.0, "Валерий: "+commands[0][1][to_send]+"\n")
+                number = 5
+                text.insert(1.0, "Валерий: "+commands[number][0]+"\n")
+                text.insert(1.0, "Валера: выберите нужную команду:\n")
+                for key in commands[number][1]:
+                    text.insert(1.0, key+"\n")
+            else:
+                text.insert(1.0, 'Валера: Введите правльную команду!\n')
+        else:
+            text.insert(1.0, "Валерий: "+commands[number][1][to_send]+"\n")
+            number = 0
 
         text.config(state = 'disabled')
         message.delete(0, 'end')
@@ -101,9 +110,15 @@ def delete_everything():
 def greeting():
     button3.config(state = 'disabled')
     text.config(state = 'normal')
-    text.insert(1.0, '\n' 'Валера: Приветствую! Я - Валерий, помощник-секретарь МОУ "Лицйей№14.'\
+    
+    text.insert(1.0, "Валера: выберите нужную команду:\n")
+    for key in commands[0][1]:
+            text.insert(1.0, key+"\n")
+    text.insert(1.0, '\n' 'Валера: Приветствую! Я - Валерий, помощник-секретарь МОУ "Лицей №14.'\
         '\n' 'Вот список того, что я умею:'\
             '\n' '- Введите ')
+    global number
+    number = 0
     text.tag_add('Color', 1.0, "1.6")
     text.tag_config('Color', foreground = 'green', font = ('Courier', 14, 'bold'))
  
@@ -135,7 +150,7 @@ scroll.grid(row = 1, column = 3, rowspan = 3,  stick = 'ns')
 text.config(yscrollcommand = scroll.set)
 
 #main = iteration()
-print(iteration.commands)
+#print(iteration.commands)
 #main = iteration()
 #main(main(0))
 #sleep(180)
