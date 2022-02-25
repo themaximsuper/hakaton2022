bfa2a930c64395a980aa9bf1addb5177931608bc
Maxim <41567448+themaximsuper@users.noreply.github.com>
2022-02-25 18:58:35 +0300

    Updated GUI.py

--- GUI.py
+++ GUI.py
@@ -1,4 +1,3 @@
-from time import sleep
 import tkinter
 
 #Список команд
@@ -7,7 +6,7 @@ commands = [
         {
             "Общие сведения.":"Выберите одно из интересующих вас сведений:",
             "Контакты.":"Выберите один из представленных основных контактов:",
-            "Достижения.":"c.", 
+            "Достижения.":"Выберите интересующую вас сферу достижений:", 
             "Создатели бота.":"Для большей информации выберите одну из команд. Бота создали Малинкович Максим и Бобовников Арсений.", 
             "Общая контактная информация.":"Выберите одну из команд:"
          },
@@ -18,21 +17,25 @@ commands = [
             "Дата основания.":"Лицей№14 был образован в 1992 году.", 
             "Количество учащихся.":"На данный момент в нашем лицее учатся"\
                 "около 300 человек.",
-            "Количество учителей.":"На даннный момент в лицее преподает" \
-                "25 учителей."
+            "Количество учителей.":"На даннный момент в лицее преподают" \
+                " 25 учителей."
         },
     ),
 
     (
         {
-            "Директор Пилипенко Надежда Михайловна.":"Телефон: 8 496 577 00 82.",
-            "Заместитель директора Николева Светлана Генадьевна.":"Телефон: 8 496 574 34 84.",
-            "Заместитель директора по безопасности Новиков Александр Александрович.":"Телефон: +7 496 574 21 57."
+            "Директор.":"Пилипенко Надежда Михайловна, Телефон: 8 496 577 00 82.",
+            "Заместитель директора.":"Николаева Светлана Генадьевна, Телефон: 8 496 574 34 84.",
+            "Заместитель директора по безопасности.":"Новиков Александр Александрович, Телефон: +7 496 574 21 57."
         },
     ),
 
     (
-        {"1.3.1":"ca.", "1.3.2":"cb.", "1.3.3":"cc."},
+        {
+            "Топ по МСК области.":"МОУ'Лицей№14' входит в Топ-100 школ Подмосковья с 2018 года.",
+            "Активность.":"Ученики Лицея активно принимают участие в разных волонтерских и конкурсных мероприятиях.",
+            "Всероссийские олимпиады.":"Каждый год ученики МОУ'Лице№14' активно принимают участие и занимают призовые места во ВсОШ по всем предметам."
+        },
     ),
             
     (
@@ -43,7 +46,8 @@ commands = [
         },
     ),
 
-    ( 
+    (
+        
         {
             "Адрес.":"144000, Московская обл., г.Электросталь, проезд Чернышевского, д.22.",
             "Адрес сайта.":"https://estalsch14.edumsko.ru/",
@@ -60,21 +64,54 @@ global number
 number = 0
 win.geometry(f"{w}x{h}")
 win.title('Бот Валера.')
+photo = tkinter.PhotoImage(file = 'bot.png')
+win.iconphoto(False, photo)
 win.resizable(False, True)
 
 #Создаем вспомогательное окно.
 class instruction(tkinter.Toplevel):
     def __init__(self, parent):
         super().__init__(parent)
-        self.text1 = tkinter.Text(self, width = 35, height = 15, font = 'Arial', wrap = 'word')
-        self.text1.config(yscrollcommand = 'scroll.set', state = 'disabled')
-        self.text1.grid(row = 1, column = 1)
-        self.button5 = tkinter.Button(self, text="Закрыть", command=self.destroy)
-        self.button5.grid(row = 3, column = 2)
+        self.instr_text = tkinter.Text(self, width = 50, height = 15, font = 'Courier', wrap = 'word')
+        self.instr_text.config(yscrollcommand = 'scroll.set')
+        self.instr_text.grid(row = 1, column = 1)
+        self.instr_text.insert(1.0,  
+                          'В данном руководстве вы можете найти все команды Бота Валеры.\n'\
+                          'Все доступные вам команды выводятся на экран.\n'\
+                          'Чтобы начать вводить команды, нажмите на кнопку "Начать диалог."\n'\
+                          'Чтобы ввести команду, кликните по полю ввода сверху и начните вводить команду в формате, указанном ботом.\n'\
+                          'Нажмите кнопку "Отправить", чтобы подтвердить ввод команды.\n'\
+                          'Колесиком мыши вы можете листать чат с ботом.\n'\
+"""    Команды первого уровня:
+-Общие сведения.
+-Контакты.
+-Достижения.
+-Создатели бота.
+-Общая контактная информация.
+    Команды второго уровня:
+-Дата основания.
+-Количество учащихся.
+-Количество учителей.
+-Директор.
+-Заместитель директора.
+-Заместитель директора по безопасности.
+-Эл.Почта.
+-Номер телефона.
+-Класс.
+-Адрес.
+-Адрес сайта.
+-Телефон.
+-Топ по МСК области.
+-Активность.
+-Всероссийские олимпиады."""
+                                )
+        self.close_btn = tkinter.Button(self, text = "Закрыть", command = self.destroy)
+        self.close_btn.grid(row = 3, column = 2)
         self.scroll1 = tkinter.Scrollbar(self)
         self.scroll1.grid(row = 0, column = 2, rowspan = 3, stick = 'ns')
-        
-        
+        self.instr_label2 = tkinter.Label(self, text = '---Инструкция---')
+        self.instr_label2.grid(row = 0, column = 1, columnspan = 2, stick = 'we')
+        self.instr_text.config(state = 'disabled')
 
 
 
@@ -87,40 +124,40 @@ def get_entry():
     if to_send:
         text.insert(1.0, '\n' f'Вы: {to_send}''\n')
         
-        #Просчет разветвлений
+        #Просчет разветвлений.
         if number == 0:
             if to_send == "Общие сведения.":
-                text.insert(1.0, "Валера: "+commands[0][0][to_send]+"\n")
+                text.insert(1.0,'\n' "Валера: "+commands[0][0][to_send]+"\n")
                 number = 1
                 for key in commands[number][0]:
                     text.insert(1.0, key+"\n")
                 text.config(state = 'disabled')
             elif to_send == "Контакты.":
-                text.insert(1.0, "Валера: "+commands[0][0][to_send]+"\n")
+                text.insert(1.0,'\n' "Валера: "+commands[0][0][to_send]+"\n")
                 number = 2
                 for key in commands[number][0]:
                     text.insert(1.0, key+"\n")
                 text.config(state = 'disabled')
             elif to_send == "Достижения.":
-                text.insert(1.0, "Валера: "+commands[0][0][to_send]+"\n")
+                text.insert(1.0,'\n' "Валера: "+commands[0][0][to_send]+"\n")
                 number = 3
                 for key in commands[number][0]:
                     text.insert(1.0, key+"\n")
                 text.config(state = 'disabled')
             elif to_send == "Создатели бота.":
-                text.insert(1.0, "Валера: "+commands[0][0][to_send]+"\n")
+                text.insert(1.0,'\n' "Валера: "+commands[0][0][to_send]+"\n")
                 number = 4
                 for key in commands[number][0]:
                     text.insert(1.0, key+"\n")
                 text.config(state = 'disabled')
             elif to_send == "Общая контактная информация.":
-                text.insert(1.0, "Валера: "+commands[0][0][to_send]+"\n")
+                text.insert(1.0,'\n' "Валера: "+commands[0][0][to_send]+"\n")
                 number = 5
                 for key in commands[number][0]:
                     text.insert(1.0, key+"\n")
                 text.config(state = 'disabled')
             else:
-                text.insert(1.0, 'Валера: Введите правильную команду!\n')
+                text.insert(1.0,'\n' 'Валера: Введите правильную команду!\n')
                 text.config(state = 'disabled')
         #Проверка правильности ввода.
         else:
@@ -139,16 +176,16 @@ def get_entry():
 def delete_everything():
     global number
     number = 0
-    button3.config(state = 'normal')
+    start_btn.config(state = 'normal')
     text.config(state = 'normal')
     text.delete(1.0, 'end')
     text.config(state = 'disabled')
-    button1.config(state = 'disabled')
+    send_btn.config(state = 'disabled')
 
 #Начало диалога.
 def greeting():
-    button3.config(state = 'disabled')
-    button1.config(state = 'normal')
+    start_btn.config(state = 'disabled')
+    send_btn.config(state = 'normal')
     text.config(state = 'normal')
     
     text.insert(1.0, 
@@ -164,31 +201,32 @@ def greeting():
     text.config(state = 'disabled')
 
 #Функционал кнопки вызова доп. окна.
-def instr_button():
+def instr_func():
     global win
     instr_win = instruction(win)
     instr_win.grab_set()
+    
 
     
  
-#Все надписи, кнопки, поля ввода и текстовые поля.
-label1 = tkinter.Label(win, text = 'Сообщение:')
-label1.grid(row = 0, column = 0)
+#Объявление всех кнопок, надписей, полей ввода и текстовых полей.
+send_label = tkinter.Label(win, text = 'Сообщение:')
+send_label.grid(row = 0, column = 0)
 
 message = tkinter.Entry(win)
 message.grid(row = 0, column = 1, columnspan = 9, stick = 'we')
 
-button1 = tkinter.Button(win, text = 'Отправить.', command = get_entry, state = 'disabled')
-button1.grid(row = 0, column = 10, columnspan = 2, stick = 'we')
+send_btn = tkinter.Button(win, text = 'Отправить.', command = get_entry, state = 'disabled')
+send_btn.grid(row = 0, column = 10, columnspan = 2, stick = 'we')
                                                                                                                                                                                                         
-button2 = tkinter.Button(win, text = 'Очистить чат.', command = delete_everything)
-button2.grid(row = 5, column = 1, columnspan = 1, stick = 'we')
+eraze_btn = tkinter.Button(win, text = 'Очистить чат.', command = delete_everything)
+eraze_btn.grid(row = 5, column = 1, columnspan = 1, stick = 'we')
 
-button3 = tkinter.Button(win, text = 'Начать диалог.', command = greeting)
-button3.grid(row = 5, column = 0, stick = 'we')
+start_btn = tkinter.Button(win, text = 'Начать диалог.', command = greeting)
+start_btn.grid(row = 5, column = 0, stick = 'we')
 
-button4 = tkinter.Button(win, text = 'Инструкция.', command = instr_button)
-button4.grid(row = 1, column = 0, columnspan = 1, stick = 'ns')
+instr_btn = tkinter.Button(win, text = 'Инструкция.', command = instr_func)
+instr_btn.grid(row = 1, column = 0, columnspan = 1, stick = 'ns')
 
 text = tkinter.Text(win, width = 55, height = 25, font = 'Arial', wrap = 'word')
 text.config(state = 'disabled')
